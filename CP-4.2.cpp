850304f6fb3f5fac42a1664b6d5ac5edd14db43d
MohammadFayan Suthar <fayansuthar26@gmail.com>
2026-08-18 23:54:01 +0530

    CP Practical 4.2

---
+++ CP-4.2.cpp
@@ -0,0 +1,22 @@
+#include<iostrem>
+using namespace std;
+
+int main(){
+
+    string s,p;
+
+    cin>>s;
+    cin>>p;
+
+    int i=0;
+    int j=0;
+
+    int match =0;
+    int star =-1;
+
+    if()
+
+
+   return 0;
+
+}
